d523a231300c2881897d3c25aa56081ac18a9762
Lucia Nicole Rosette Hernández <luciarosette1626@gmail.com>
2019-11-09 12:23:16 -0600

    tarea7y8

--- TAREAS/7/main.c
+++ TAREAS/7/main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#include <math.h> //para poder hacer operaciones matemáticas
 #include <stdlib.h>
-
-int main (int argc, char*argv[]){
-double operacion, base, potencia; //declarar variables con double para usar decimales
-//atof para convertir el argumento a número
-base=atof(argv[1]); 
-potencia=atof (argv[2]);
-operacion=pow(base,potencia); //pow se utiliza para hacer potencias
-printf("%.2f\n",operacion );
+int main (int arcgc, char*argu[]){
+float num1, num2, resultado=1;
+//atof para convertir a numero
+num1=atof(argu[1]);
+num2=atof(argu[2]);
+//Hacer la potencia con multiplicaciones
+//Repetir el proceso hasta que i sea menor al numero dos 
+for (int i=0; i<num2; i++){
+	resultado=resultado*num1;
+}
+printf("%.2f\n", resultado );
 return 0;
 }
